076902055d4229fb8daa0127095d3ac992483751
rachelpeles <197303r@gmail.com>
2021-10-05 18:37:59 +0300

    05/10 add a navigete from material/ui

--- src/components/TopManu.tsx
+++ src/components/TopManu.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
+export default function TopManu() {
+  const [value, setValue] = React.useState(0);
 
-function TopManu(){
-    return (<div>
-
-    </div>)
+  return (
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="homePage" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="delivery" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="sign in" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
 }
-export default TopManu
